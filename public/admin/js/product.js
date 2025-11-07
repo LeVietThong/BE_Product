2752fb16a02b4111dc981f83a59127f4a04001c1
LeVietThong <levietthong92@gmail.com>
2025-11-07 15:11:50 +0700

    logic change_status

---
+++ public/admin/js/product.js
@@ -0,0 +1,20 @@
+//ChangeStatus
+const btnChangeStatus = document.querySelectorAll("[button-change-status]");
+if(btnChangeStatus.length > 0) {
+  const formChangeStatus = document.querySelector("#form-change-status");
+  const path = formChangeStatus.getAttribute("data-path");
+
+  btnChangeStatus.forEach(button => {
+    button.addEventListener("click", () => {
+      const statusCurrent = button.getAttribute("data-status");
+      const id = button.getAttribute("data-id");
+
+      let statusChange = statusCurrent == "active" ? "inactive" : "active";
+
+      const action = path + `/${statusChange}/${id}?_method=PATCH`;
+      formChangeStatus.action = action;
+
+      formChangeStatus.submit();
+    });
+  });
+}
