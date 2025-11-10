dedd999855161ac3341d93ca7944026ccc29e237
LeVietThong <levietthong92@gmail.com>
2025-11-10 14:39:12 +0700

    create_product

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,5 +1,7 @@
 const Product = require("../../models/product.model");
 
+const systemConfig = require("../../config/system");
+
 const filterStatusHelper = require("../..//helpers/filterStatus");
 
 const searchHelper = require("../../helpers/search");
@@ -71,11 +73,17 @@ module.exports.changeMulti = async (req, res) => {
   switch (type) {
     case "active":
       await Product.updateMany({ _id: { $in: ids } }, { status: "active" });
-      req.flash("sucess", `Cập nhật trạng thái thành công ${ids.length} sản phẩm!`);
+      req.flash(
+        "sucess",
+        `Cập nhật trạng thái thành công ${ids.length} sản phẩm!`
+      );
       break;
     case "inactive":
       await Product.updateMany({ _id: { $in: ids } }, { status: "inactive" });
-      req.flash("sucess", `Cập nhật trạng thái thành công ${ids.length} sản phẩm!`);
+      req.flash(
+        "sucess",
+        `Cập nhật trạng thái thành công ${ids.length} sản phẩm!`
+      );
       break;
     case "delete-all":
       await Product.updateMany(
@@ -119,4 +127,30 @@ module.exports.deleteItem = async (req, res) => {
   );
 
   res.redirect("/admin/products");
+};
+
+//[GET] /admin/products/create
+module.exports.create = async (req, res) => {
+  res.render("admin/pages/products/create", {
+    pageTitle: "Thêm mới sản phẩm",
+  });
+};
+
+//[POST] /admin/products/create
+module.exports.createPost = async (req, res) => {
+  req.body.price = parseInt(req.body.price);
+  req.body.discountPercentage = parseInt(req.body.discountPercentage);
+  req.body.stock = parseInt(req.body.stock);
+
+  if (req.body.position == "") {
+    const countProducts = await Product.countDocuments();
+    req.body.position = countProducts + 1;
+  } else {
+    req.body.position = parseInt(req.body.position);
+  }
+
+  const product = new Product(req.body);
+  await product.save();
+
+  res.redirect(`${systemConfig.prefixAdmin}/products`);
 };
--- models/product.model.js
+++ models/product.model.js
@@ -1,17 +1,33 @@
 const mongoose = require("mongoose");
+const slug = require("mongoose-slug-updater");
 
-const productsSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  price: Number,
-  discountPercentage: Number,
-  stock: Number,
-  status: String,
-  position: Number,
-  thumbnail: String,
-  delete: Boolean,
-  deleteAt: Date
-});
+mongoose.plugin(slug);
+
+const productsSchema = new mongoose.Schema(
+  {
+    title: String,
+    description: String,
+    price: Number,
+    discountPercentage: Number,
+    stock: Number,
+    status: String,
+    position: Number,
+    slug: {
+      type: String,
+      slug: "title",
+      unique: true,
+    },
+    thumbnail: String,
+    delete: {
+      type: Boolean,
+      default: false,
+    },
+    deleteAt: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Product = mongoose.model("Product", productsSchema, "products");
 
