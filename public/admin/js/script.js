7b78b5baa9e5bea18395b074ccded3a6ffec965b
LeVietThong <levietthong92@gmail.com>
2025-11-10 16:27:44 +0700

    update upload_file

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -139,16 +139,32 @@ if (formChangeMulti) {
 
 //Show Alert
 const showAlert = document.querySelector("[show-alert]");
-  if (showAlert) {
-    const time = showAlert.getAttribute("data-time");
-    const closeAlert = showAlert.querySelector("[close-alert]");
+if (showAlert) {
+  const time = showAlert.getAttribute("data-time");
+  const closeAlert = showAlert.querySelector("[close-alert]");
 
-    setTimeout(() => {
-      showAlert.classList.add("alert-hidden");
-    }, time);
+  setTimeout(() => {
+    showAlert.classList.add("alert-hidden");
+  }, time);
 
-    closeAlert.addEventListener("click", () => {
-      showAlert.classList.add("alert-hidden");
-    });
-  }
+  closeAlert.addEventListener("click", () => {
+    showAlert.classList.add("alert-hidden");
+  });
+}
+//End
+
+//Upload Image
+const uploadImage = document.querySelector("[upload-image]");
+if (uploadImage) {
+  const uploadImageInput = document.querySelector("[upload-image-input]");
+  const uploadImagePreview = document.querySelector("[upload-image-preview]");
+
+  uploadImageInput.addEventListener("change", () => {
+    console.log(e);
+    const file = e.target.files[0];
+    if (file) {
+      uploadImagePreview.src = URL.createObjectURL(file);
+    }
+  });
+}
 //End
