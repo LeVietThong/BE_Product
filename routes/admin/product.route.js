2830f8683ed30d5e3e89ea4ccb13556b9a79e18e
LeVietThong <levietthong92@gmail.com>
2025-11-11 14:45:05 +0700

    import file cloud

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const multer = require("multer");
 const route = express.Router();
-const storageMulter = require("../../helpers/storageMulter");
-const upload = multer({ storage: storageMulter() });
+
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
+
+const upload = multer();
 const controller = require("../../controllers/admin/product.controller");
 const validate = require("../../validates/admin/product.validate");
 
@@ -18,11 +20,13 @@ route.get("/create", controller.create);
 route.post(
   "/create",
   upload.single("thumbnail"),
+  uploadCloud.upload,
   validate.createPost,
   controller.createPost
 );
 
 route.get("/edit/:id", controller.edit);
+
 route.patch(
   "/edit/:id",
   upload.single("thumbnail"),
