b35ef72a71e50c7b51bdc71ad7bd0feaf514d5f2
LeVietThong <levietthong92@gmail.com>
2025-11-11 10:52:25 +0700

    detail_item client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -5,7 +5,7 @@ module.exports.index = async (req, res) => {
   const products = await Product.find({
     status: "active",
     delete: false,
-  }).sort({position: "desc"});
+  }).sort({ position: "desc" });
 
   const newProducts = products.map((item) => {
     item.priceNew = (
@@ -22,4 +22,24 @@ module.exports.index = async (req, res) => {
     pageTitle: "Trang danh sách sản phẩm",
     products: newProducts,
   });
+};
+
+// [GET] /products/:slug
+module.exports.detail = async (req, res) => {
+  try {
+    const find = {
+      delete: false,
+      slug: req.params.id,
+      status: "active",
+    };
+
+    const product = await Product.findOne(find);
+
+    res.render("client/pages/products/detail", {
+      pageTitle: product.title,
+      product: product,
+    });
+  } catch (error) {
+    res.redirect(`/products`);
+  }
 };
