c04a00ea1519d7ce614c620e70b6a066438288b4
LeVietThong <levietthong92@gmail.com>
2025-11-13 17:15:06 +0700

    thêm mới danh mục sản phẩm ở product

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,5 +1,7 @@
 const Product = require("../../models/product.model");
 
+const ProductCategory = require("../../models/product-category.model");
+
 const systemConfig = require("../../config/system");
 
 const filterStatusHelper = require("../../helpers/filterStatus");
@@ -7,6 +9,8 @@ const filterStatusHelper = require("../../helpers/filterStatus");
 const searchHelper = require("../../helpers/search");
 
 const paginationHelper = require("../../helpers/pagination");
+
+const createTreeHelper = require("../../helpers/createTree");
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
@@ -142,8 +146,15 @@ module.exports.deleteItem = async (req, res) => {
 
 //[GET] /admin/products/create
 module.exports.create = async (req, res) => {
+  let find = {
+    delete: false,
+  };
+  const category = await ProductCategory.find(find);
+
+  const newCategory = createTreeHelper.tree(category);
   res.render("admin/pages/products/create", {
     pageTitle: "Thêm mới sản phẩm",
+    category: newCategory,
   });
 };
 
