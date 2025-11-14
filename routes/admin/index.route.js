6f67c3ecfa47ce5fc95756eb3ed469a78edf0795
LeVietThong <levietthong92@gmail.com>
2025-11-14 10:01:13 +0700

    xây dựng phần ds nhóm phân quyền

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -3,11 +3,16 @@ const systemConfig = require("../../config/system")
 const dashboardRoute = require("./dashboard.route");
 const productRoute = require("./product.route");
 const productsCategoryRoute = require("./product-category.route");
+const roleRoute = require("./role.route");
 
 module.exports = (app) => {
   const PATH_ADMIN = systemConfig.prefixAdmin;
 
   app.use(PATH_ADMIN + "/dashboard", dashboardRoute);
+
   app.use(PATH_ADMIN + "/products", productRoute);
+
   app.use(PATH_ADMIN + "/products-category", productsCategoryRoute);
+  
+  app.use(PATH_ADMIN + "/roles", roleRoute);
 };
