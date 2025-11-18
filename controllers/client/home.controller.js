8ddc9cee730eeefbe2fd7a48f1f9b5e92ca3c42a
LeVietThong <levietthong92@gmail.com>
2025-11-18 15:00:53 +0700

    hiển thị danh sách item nổi bật

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,6 +1,19 @@
+const Product = require("../../models/product.model");
+
+const productsHelper = require("../../helpers/products");
+
 // [GET] /
 module.exports.index = async (req, res) => {
+  const productsFeatured = await Product.find({
+    featured: "1",
+    delete: false,
+    status: "active",
+  }).limit(5);
+
+  const newProducts = productsHelper.priceNewProducts(productsFeatured);
+
   res.render("client/pages/home/index", {
     pageTitle: "Trang chủ",
+    productsFeatured: newProducts,
   });
 };
