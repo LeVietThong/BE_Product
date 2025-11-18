dc90c3138b018da6c5268fea767a3459ce8d3d66
LeVietThong <levietthong92@gmail.com>
2025-11-18 16:02:56 +0700

    lấy danh sách sản phẩm client

--- routes/client/product.route.js
+++ routes/client/product.route.js
@@ -5,6 +5,8 @@ const controller = require("../../controllers/client/product.controller");
 
 route.get("/", controller.index);
 
-route.get("/:slug", controller.detail);
+route.get("/:slugCategory", controller.category);
+
+// route.get("/:slug", controller.detail);
 
 module.exports = route;
