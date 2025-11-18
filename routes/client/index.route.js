0b69eb90c291d5de0e450994de3b538a79b1a125
LeVietThong <levietthong92@gmail.com>
2025-11-18 11:10:14 +0700

    show menu client

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,7 +1,11 @@
+const categoryMiddlewareRoute = require("../../middlewares/client/category.middleware");
+
 const productRoute = require("./product.route");
 const homeRoute = require("./home.route");
 
 module.exports = (app) => {
+  app.use(categoryMiddlewareRoute.category);
+
   app.use("/", homeRoute);
 
   app.use("/products", productRoute);
