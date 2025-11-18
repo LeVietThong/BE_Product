71372e4eb5da115a62f9d84be969ea9bf0137d96
LeVietThong <levietthong92@gmail.com>
2025-11-18 15:08:53 +0700

    hiển thị sản phẩm mới

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -4,16 +4,26 @@ const productsHelper = require("../../helpers/products");
 
 // [GET] /
 module.exports.index = async (req, res) => {
+  //Lấy ra sản phẩm nổi bật
   const productsFeatured = await Product.find({
     featured: "1",
     delete: false,
     status: "active",
   }).limit(5);
 
-  const newProducts = productsHelper.priceNewProducts(productsFeatured);
+  const newProductsFeatured = productsHelper.priceNewProducts(productsFeatured);
+
+  //Lấy ra danh sách sản phẩm mới nhất
+  const productsNew = await Product.find({
+    status: "active",
+    delete: false,
+  }).sort({ position: "desc" }).limit(5);
+
+  const newProductsNew = productsHelper.priceNewProducts(productsNew);
 
   res.render("client/pages/home/index", {
     pageTitle: "Trang chủ",
-    productsFeatured: newProducts,
+    productsFeatured: newProductsFeatured,
+    productsNew: newProductsNew,
   });
 };
