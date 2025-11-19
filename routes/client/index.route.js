d18178867162c513c77a3cf862a7630b3bd20f86
LeVietThong <levietthong92@gmail.com>
2025-11-19 10:21:40 +0700

    logic search item

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -2,11 +2,14 @@ const categoryMiddlewareRoute = require("../../middlewares/client/category.middl
 
 const productRoute = require("./product.route");
 const homeRoute = require("./home.route");
+const searchRoute = require("./search.route");
 
 module.exports = (app) => {
   app.use(categoryMiddlewareRoute.category);
 
   app.use("/", homeRoute);
+
+  app.use("/search", searchRoute);
 
   app.use("/products", productRoute);
 };
