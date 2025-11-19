b99673bc6741ef3579d6612fda6bebb5c3866906
LeVietThong <levietthong92@gmail.com>
2025-11-19 15:30:09 +0700

    thêm sản phẩm vào giỏ hàng

---
+++ controllers/client/cart.controller.js
@@ -0,0 +1,38 @@
+const Cart = require("../../models/cart.model");
+
+//[POST] /cart/add/:productId
+module.exports.addPost = async (req, res) => {
+  const productId = req.params.productId;
+  const quantity = parseInt(req.body.quantity);
+  const cartId = req.cookies.cartId;
+
+  const cart = await Cart.findOne({
+    _id: cartId,
+  });
+
+  console.log(cartId);
+
+  console.log(cart);
+
+  const existProductInCart = cart.products.find(
+    (item) => item.product_id == productId
+  );
+
+  if (existProductInCart) {
+    const quantityNew = existProductInCart.quantity + quantity;
+
+    await Cart.updateOne(
+      { _id: cartId, "products.product_id": productId },
+      { $set: { "products.$.quantity": quantityNew } }
+    );
+  } else {
+    const objectCart = {
+      product_id: productId,
+      quantity: quantity,
+    };
+    await Cart.updateOne({ _id: cartId }, { $push: { products: objectCart } });
+    req.flash("success", "Đã thêm sản phẩm vào giỏ hàng!");
+
+  }
+  res.redirect(`/products/detail/${req.params.title}`);
+};
--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,15 +1,20 @@
 const categoryMiddlewareRoute = require("../../middlewares/client/category.middleware");
+const cartMiddlewareRoute = require("../../middlewares/client/cart.middleware");
 
 const productRoute = require("./product.route");
 const homeRoute = require("./home.route");
 const searchRoute = require("./search.route");
+const cartRoute = require("./cart.route");
 
 module.exports = (app) => {
   app.use(categoryMiddlewareRoute.category);
+  app.use(cartMiddlewareRoute.cartId);
 
   app.use("/", homeRoute);
 
   app.use("/search", searchRoute);
 
   app.use("/products", productRoute);
+
+  app.use("/cart", cartRoute);
 };
