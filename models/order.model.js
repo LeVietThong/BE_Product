34be829e944ee581be7870ff0c08896cc99b4904
LeVietThong <levietthong92@gmail.com>
2025-11-20 10:39:01 +0700

    collection & model order

---
+++ models/order.model.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+  cart_id: String,
+  userInfo: {
+    fullName: String,
+    phone: String,
+    address: String,
+  },
+  products: [
+    {
+      productId: String,
+      price: Number,
+      discountPercentage: Number,
+      quantity: Number
+    }
+  ],
+  delete: {
+    type: Boolean,
+    default: false
+  },
+  deleteAt: Date,
+},
+{
+  timestamps: true
+});
+
+const Order = mongoose.model("Order", orderSchema, "orders");
+
+module.exports = Order;
