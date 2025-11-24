be4ecfa30a4ce9ae107062e5cd10bac7490e5624
LeVietThong <levietthong92@gmail.com>
2025-11-24 15:05:47 +0700

    trang 404

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const path = require('path');
+const path = require("path");
 const methodOverride = require("method-override");
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 const session = require("express-session");
-const flash = require('express-flash');
-const moment = require('moment');
+const flash = require("express-flash");
+const moment = require("moment");
 require("dotenv").config();
 
 const database = require("./config/database");
@@ -30,13 +30,16 @@ app.set("view engine", "pug");
 app.use(express.static(`${__dirname}/public`));
 
 //Express-flash
-app.use(cookieParser('keyboard cat'));
-app.use(session({ cookie: { maxAge: 60000 }}));
+app.use(cookieParser("keyboard cat"));
+app.use(session({ cookie: { maxAge: 60000 } }));
 app.use(flash());
 
 //TinyMCE
 
-app.use("/tinymce", express.static(path.join(__dirname, "node_modules", "tinymce")));
+app.use(
+  "/tinymce",
+  express.static(path.join(__dirname, "node_modules", "tinymce"))
+);
 
 //App Local Variables
 app.locals.prefixAdmin = systemConfig.prefixAdmin;
@@ -45,6 +48,12 @@ app.locals.moment = moment;
 //Routes
 routeAdmin(app);
 route(app);
+
+app.get(/.*/, (req, res) => {
+  res.render("client/pages/errors/404", {
+    pageTitle: "404 Not Found",
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
