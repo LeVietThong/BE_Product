5ac45794602503aadbfe30993ba5eba9c25062f1
LeVietThong <levietthong92@gmail.com>
2025-12-01 15:26:42 +0700

    fix lỗi upload image

--- middlewares/admin/uploadCloud.middleware.js
+++ middlewares/admin/uploadCloud.middleware.js
@@ -2,7 +2,7 @@ const uploadToCloud = require("../../helpers/uploadToCloud");
 
 module.exports.upload = async (req, res, next) => {
   if (req.file) {
-    const link = await uploadToCloud(req);
+    const link = await uploadToCloud(req.file.buffer);
     req.body[req.file.fieldname] = link;
   }
 
